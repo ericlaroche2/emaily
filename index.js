82d2dfb9f96ee339b811ac99813fa808d0e22142
Eric Laroche <ericlaroche2@gmail.com>
2018-07-15 13:31:32 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+//import express from "express"; //es 2015 syntax
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+//HEROKU DEPLOYMENT
+//1. dynamic port binding
+const PORT = process.env.PORT || 5001; //prod or dev
+
+//2. specifify node env
+// specify engine in package.json
+
+//3. specify start script
+//package.json script, modify to node index.js
+
+//4. create ignore file
+//
+
+app.listen(5001);
