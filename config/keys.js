7af685e76c0ef5f6ff4963ee5c7c46db422d35b4
Eric Laroche <ericlaroche2@gmail.com>
2018-08-19 18:43:14 -0400

    key path fix

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,12 @@
 
 //
 console.log('process env',process.env);
-if (process.env.NODE === 'production') { //automatically set on heroku
+if (process.env.NODE_ENV === 'production') { //automatically set on heroku
   module.exports = require('./prod');
+  console.log('process env','prod');
+
 } else {
   module.exports = require('./dev');
+  console.log('process env dev');
+
 }
