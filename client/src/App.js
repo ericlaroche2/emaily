5200af6f4e46e0003aa19b617fb63dde3b4764c0
Eric Laroche <ericlaroche2@gmail.com>
2018-08-27 21:53:08 -0400

    concurrently and react dev/prod routing

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">hey</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="/auth/google">a link to google auth</a>
+      </div>
+    );
+  }
+}
+
+export default App;
+//run client and server together
+//npm run server, npm run dev
